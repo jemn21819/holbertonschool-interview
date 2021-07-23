a83c069e9c2d1217c2cb7d825214cd16452c2d74
James <jmartinez21834@stu.nuc.edu>
2021-07-23 02:04:41 +0000

    initial commit

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -5,21 +5,12 @@ Write a method that determines if all the boxes can be opened
 
 
 def canUnlockAll(boxes):
-    if len(boxes) == 0:
-        return True
     keys = [0]
-    open_box = [0]
-    add_key = True
-    while add_key:
-        add_key = False
-        for key in boxes:
-            if boxes.index(key) in keys:
-                if boxes.index(key) not in open_box:
-                    open_box.append(boxes.index(key))
-                for num in key:
-                    if num not in keys:
-                        keys.append(num)
-                        add_key = True
-    if len(open_box) == len(boxes):
+    for num in keys:
+        for key in boxes[num]:
+            if key not in keys:
+                if key < len(boxes):
+                    keys.append(key)
+    if len(keys) == len(boxes):
         return True
     return False
